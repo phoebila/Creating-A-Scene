7be0c55fe9525f8c00e3e6c351cdc970cda65c02
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-01-17 15:05:24 -0800

    adding base

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Phoebe Royer
+// Date: 1/17/23
+// I'm here
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO
+}
+
+let game = new Phaser.Game(config)
